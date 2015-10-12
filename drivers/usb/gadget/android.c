508426425f95289aef369a8fa2f8c867bea6a986
mv <michvett.mv@gmail.com>
2015-10-12 12:21:59 +0200

    Revert "usb_gadget: update: patch: support for usb keyboard app from pelya"
    maybe causes adb problems
    
    This reverts commit 4899a76418ff27eae9a7307415e2dc8cf260b9bc.
    
    Conflicts:
    	drivers/usb/gadget/android.c

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -68,9 +68,6 @@
 #include "f_ccid.c"
 #include "f_mtp.c"
 #include "f_accessory.c"
-#include "f_hid.h"
-#include "f_hid_android_keyboard.c"
-#include "f_hid_android_mouse.c"
 #ifdef CONFIG_USB_ANDROID_CDC_ECM
 #include "f_ecm.c"
 #else
@@ -1819,41 +1816,6 @@ static struct android_usb_function uasp_function = {
 	.bind_config	= uasp_function_bind_config,
 };
 
-static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
-{
-	return ghid_setup(cdev->gadget, 2);
-}
-
-static void hid_function_cleanup(struct android_usb_function *f)
-{
-	ghid_cleanup();
-}
-
-static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
-{
-	int ret;
-	printk(KERN_INFO "hid keyboard\n");
-	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
-		return ret;
-	}
-	printk(KERN_INFO "hid mouse\n");
-	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
-	if (ret) {
-		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
-		return ret;
-	}
-	return 0;
-}
-
-static struct android_usb_function hid_function = {
-	.name		= "hid",
-	.init		= hid_function_init,
-	.cleanup	= hid_function_cleanup,
-	.bind_config	= hid_function_bind_config,
-};
-
 static int midi_function_init(struct android_usb_function *f,
 					struct usb_composite_dev *cdev)
 {
@@ -1934,7 +1896,6 @@ static struct android_usb_function *supported_functions[] = {
 	&audio_source_function,
 	&midi_function,
 	&uasp_function,
-	&hid_function,
 	NULL
 };
 
@@ -2208,8 +2169,6 @@ functions_store(struct device *pdev, struct device_attribute *attr,
 						name);
 			}
 		}
-		/* HID driver always enabled, it's the whole point of this kernel patch */
-		android_enable_function(dev, conf, "hid");
 	}
 
 	/* Free uneeded configurations if exists */
